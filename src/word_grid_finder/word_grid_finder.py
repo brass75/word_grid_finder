615d83a626889206698d41e74e9c8b56f041e067
Dan Shernicoff <dshernicoff@tenable.com>
2026-01-05 09:17:55 -0500

    Add double letter handling, dynamic terminal width

--- src/word_grid_finder/word_grid_finder.py
+++ src/word_grid_finder/word_grid_finder.py
@@ -1,3 +1,5 @@
+import shutil
+
 from abc import abstractmethod
 from dataclasses import dataclass, field
 from dykes import parse_args, StoreTrue
@@ -44,6 +46,17 @@ class WGFArgs:
         "Word must comtain multiple instances of the letter",
         Flags("-cm", "--multiple"),
     ] = None
+    double: Annotated[
+        StoreTrue,
+        "Word must contain the letter as a double letter.",
+        Flags("-d", "--double"),
+    ] = False
+    not_contain: Annotated[
+        str,
+        "Substrings that the word must not contain",
+        NArgs(value="*"),
+        Flags("-dc", "--does-not-contain"),
+    ] = field(default_factory=list)
     reversed: Annotated[
         StoreTrue,
         "Sort from long to short",
@@ -53,6 +66,8 @@ class WGFArgs:
 
 
 class Test:
+    """Base class for checks"""
+
     @abstractmethod
     def check(self, word: str) -> bool:
         """Abstract check method"""
@@ -61,10 +76,13 @@ class Test:
 
 @dataclass
 class Contains(Test):
-    substring: str
-    starts: bool = False
-    ends: bool = False
-    multiple: bool = False
+    """Check for words that contain a substring"""
+
+    substring: str  # The substring to check for
+    starts: bool = False  # If the substring needs to be at the start of the word
+    ends: bool = False  # If the substring needs to be at the end of the word
+    multiple: bool = False  # If the substring needs to appear multiple times
+    does_not: bool = False  # If the word cannot include the substring
 
     def check(self, word: str) -> bool:
         if self.starts:
@@ -73,16 +91,30 @@ class Contains(Test):
             return word.endswith(self.substring)
         if self.multiple:
             return word.count(self.substring) > 1
+        if self.does_not:
+            return self.substring not in word
         return self.substring in word
 
 
 @dataclass
 class Length(Test):
+    """Check based on the length of the word"""
+
     min_len: int
     max_len: int
 
     def check(self, word: str) -> bool:
         return self.min_len <= len(word) <= self.max_len
+
+
+@dataclass
+class Double(Test):
+    """Check for double letters"""
+
+    def check(self, word: str) -> bool:
+        import re  # Import here for lazy import since we only need re here.
+
+        return bool(re.search(r"([a-z])\1", word))
 
 
 def handle_args() -> tuple[list[Test], Path, bool]:
@@ -99,6 +131,12 @@ def handle_args() -> tuple[list[Test], Path, bool]:
         tests.extend(map(Contains, args.contains))
     if args.multiple:
         tests.append(Contains(args.multiple, multiple=True))
+    if args.double:
+        tests.append(Double())
+    if args.not_contain:
+        tests.extend(
+            Contains(substring, does_not=True) for substring in args.not_contain
+        )
     return tests, args.word_list, args.reversed
 
 
@@ -116,6 +154,7 @@ def format_output(wordlist: list[str], line_len: int = 120):
 
 
 def main():
+    """Main function"""
     tests, wordlist, reversed = handle_args()
     if not (word_list := wordlist.read_text().splitlines()):
         raise RuntimeError(f"Failed to read word list from {wordlist=}")
@@ -127,7 +166,10 @@ def main():
         key=lambda x: (len(x), x),
         reverse=reversed,
     )
-    print(format_output(valid_words))
+    width, _ = shutil.get_terminal_size(
+        (120, 40)
+    )  # Get the right width for the screen to maaximize output
+    print(format_output(valid_words, line_len=width))
 
 
 if __name__ == "__main__":
